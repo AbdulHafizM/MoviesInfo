f8a7ce5a71b8c5b4e2321a29ba1117af5641596b
AbdulhafizM <muibiabdulhafiz007@gmail.com>
2022-06-26 11:05:57 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,34 @@ import React, { useEffect,useState } from "react";
 import './app.css';
 import searchIcon from './search.svg';
 import MovieCard from './movie.jsx';
+import CircularProgress from '@mui/material/CircularProgress';
+import Pagination from "./pagination";
 //e21b8e79
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e21b8e79';
 
 function App(){
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false)
   const [value,setValue] = useState('')
-  const searchMovies = async (title) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cardPerPage] = useState(4);
 
+  const indexOfLastCard = currentPage * cardPerPage
+  const indexOfFirstCard = indexOfLastCard - cardPerPage
+  const currentCards = movies.slice(indexOfFirstCard, indexOfLastCard)
+  
+  const paginate = (pageNumber) => {setCurrentPage(pageNumber)}
+
+
+  const searchMovies = async (title) => {
+    setLoading(true)
     const response = await fetch(`${API_URL}&s=${title}`,{
       method : 'GET',
       mode : 'cors',
     });
     const data = await response.json()
+    setLoading(false)
     setMovies(data.Search)
   };
 
@@ -26,7 +40,7 @@ function App(){
 
   return(
     <div className="app">
-      <h1>FindMovies</h1>
+      <h1>MOVIE INFO</h1>
       <div className="search">
         <input placeholder="Search for movies" value={value} onChange={(e) => {
            setValue(e.target.value);
@@ -35,15 +49,20 @@ function App(){
           searchMovies(value)
         }}/>
       </div>
-  
+      
       {
         movies.length > 0 ? (
           <div className="container">
             {
-              movies.map((movie)=>(
-                <MovieCard key = {movies.indexOf(movie)} movie = {movie}/>
-              ))
+              loading ? (
+                  <div className="loader">
+                    <CircularProgress />
+                  </div>
+                ): currentCards.map((movie)=>(
+                  <MovieCard key = {movies.indexOf(movie)} movie = {movie}/>
+                ))
             }
+            
          </div>
         ) : (
           <div className="empty">
@@ -51,8 +70,14 @@ function App(){
             </div>
         )
       }
-
-
+      
+      {
+                  <Pagination 
+                      cardPerPage={cardPerPage} 
+                      totalCards={movies.length} 
+                      paginate={paginate}
+                  />
+              }
     </div>
   )
 }
